ddb542c3995bcffee79665fe4dbcebbf9b93a201
adam <adam.guettit@ocsbstudent.ca>
2018-02-13 13:30:35 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,12 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+-- created by : Adam
+-- created on : Feb 12
+
+-- place my name and image on screen
+-----------------------------------------------------------------------------------------
+
+print ( "adam, Guettit!")
+display.setDefault( "background", 0, 1, 0 )
+local image = display.newImageRect( "./assit/sprits/Attack__007.png", 1000, 1000 )
